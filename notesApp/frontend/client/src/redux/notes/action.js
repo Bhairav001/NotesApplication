704042c7024e730987ce6c716a2630ffcd8fd33e
Bhairav <bhairavgotam066@gmail.com>
2023-12-21 16:50:01 +0530

    added backend depoy and frontend josn deploy

--- notesApp/frontend/client/src/redux/notes/action.js
+++ notesApp/frontend/client/src/redux/notes/action.js
@@ -20,7 +20,7 @@ const editNoteSuccess = () =>{
 //common are all get request
 export const getNotes = (param={})=>(dispatch) =>{
       dispatch(getNotesRequestAction())
-      axios.get(`http://localhost:7070/notes`,param).then((res)=>{
+      axios.get(`https://notes-app-deploy.onrender.com/notes/`,param).then((res)=>{
         console.log(res)
         dispatch(getNotesSuccessAction(res.data))
       }).catch((err)=>{
@@ -29,7 +29,7 @@ export const getNotes = (param={})=>(dispatch) =>{
 }
 
 export const editNote =(id,NOTEData) =>(dispatch)=>{
-     return axios.patch(`http://localhost:7070/NOTEs/${id}`,NOTEData).then(()=>{
+     return axios.patch(`https://notes-app-deploy.onrender.com/notes/${id}`,NOTEData).then(()=>{
          dispatch(editNoteSuccess())
       })
 }
